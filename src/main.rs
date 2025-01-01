03b74b518488e7c898bda08f6661e532d789af6e
Joe Mafrici <hi@gojoe.dev>
2025-01-01 11:05:33 -0800

    send tarball to remote and run cargo build

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::path::PathBuf;
 use tar::Builder;
 
-use cloudflare_ssh::CloudflareSsh;
+use cloudflare_ssh::{bootstrap, CloudflareSsh};
 
 /// Deploy an app
 #[derive(Parser, Debug)]
@@ -45,14 +45,43 @@ fn main() {
     encoder.finish().expect("Unable to finish compression");
     println!("Tarball created");
 
+    // println!("bootstrapping deployment configurations");
+    // bootstrap("axum-hello-world").expect("Should have been able to bootstrap deployment config");
+
     let cloudflare_ssh_client =
         CloudflareSsh::new().expect("Unable to create cloudflare ssh client");
 
+    println!("creating /opt/axum-hello-world dir");
+    let result = cloudflare_ssh_client
+        .exec("sudo mkdir -p /opt/axum-hello-world")
+        .expect("Should have been able to make /opt/axum-hello-world dir");
+    println!("{}", result);
+    println!("setting ownership of /opt/axum-hello-world to deepwater");
+    let result = cloudflare_ssh_client
+        .exec("sudo chown -R deepwater:deepwater /opt/axum-hello-world")
+        .expect("Should have been able to set ownership of /opt/axum-hello-world to deepwater");
+    println!("{}", result);
     let bytes_sent = cloudflare_ssh_client
         .scp(
             "/Users/deepwater/archive.tar.gz",
-            "/home/deepwater/archive.tar.gz",
+            "/opt/axum-hello-world/archive.tar.gz",
         )
         .expect("Unable to scp tarball to remote");
     println!("sent {} bytes", bytes_sent);
+
+    println!("extracting tarball");
+    let result = cloudflare_ssh_client
+        .exec("tar -xvf /opt/axum-hello-world/archive.tar.gz -C /opt/axum-hello-world")
+        .expect("Unable to extract tarball");
+    println!("{}", result);
+
+    println!("checking cargo installation");
+    cloudflare_ssh_client
+        .exec("which cargo && cargo --version && pwd")
+        .expect("Unable to check cargo installation");
+
+    println!("running cargo build");
+    cloudflare_ssh_client
+        .exec("source $HOME/.cargo/env && cd /opt/axum-hello-world && cargo build --release")
+        .expect("Unable to cargo build");
 }
